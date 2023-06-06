75e6fee37f46117a695b35065d6085d268e681b9
mugishab2020 <mugishab2020@gmail.com>
2023-06-05 23:56:52 -0700

    The second task which is for checking if the last digit of e random number is in certain range is done

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number < 0:
+    number = -number
+last_dig = number % 10
+if number % 10  > 5:
+    print("Last digit of {} is {} and is greater than 5".format(number, last_dig))
+elif number % 10 == 0:
+     print("Last digit of {} is {} and is 0".format(number, last_dig))
+elif ((number % 10 < 6) and (number % 10 != 0)):
+     print("Last digit of {} is {} and is less than 6 and not 0".format(number, last_dig))
