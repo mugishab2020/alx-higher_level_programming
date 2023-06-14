e314576d2ab8259894d6d15626f56e86c6862011
mugishab2020 <mugishab2020@gmail.com>
2023-06-14 08:13:41 -0700

    This iss the program for sorting the keys in the dictionary given . it uses the sorted function

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    sorted_keys = sorted(a_dictionary.keys())
-    for key in sorted_keys:
-        print(key, ":", a_dictionary[key])
+    sorted_keys = list(a_dictionary.keys())
+    sorted_keys.sort()
+    for keys in sorted_keys:
+        print("{}: {}".format(keys, a_dictionary.get(keys)))
