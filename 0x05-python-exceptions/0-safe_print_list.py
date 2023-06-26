d3d5000af31c0bd863f68a87c0fee102c3e15c94
mugishab2020 <mugishab2020@gmail.com>
2023-06-26 09:31:16 -0700

    The task one is confusing somehow

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    for a in range (x):
-        try:
-            print(my_list[a], end = '')
+    try:
+        a = 0
+        for i in range(0, x):
+            print("{}".format(my_list[i]), end="")
             a += 1
-        except IndexError:
-            print()
-            return a
+    except IndexError:
+        pass
+    finally:
         print()
-        return x
+        return a
