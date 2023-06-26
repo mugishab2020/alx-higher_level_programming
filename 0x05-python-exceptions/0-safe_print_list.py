912598cc8adb4a221b411d73324c9f8aaacbe31d
mugishab2020 <mugishab2020@gmail.com>
2023-06-26 09:24:32 -0700

    still tring to make task 0'

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,9 +3,9 @@ def safe_print_list(my_list=[], x=0):
     for a in range (x):
         try:
             print(my_list[a], end = '')
+            a += 1
         except IndexError:
             print()
             return a
         print()
-
         return x
