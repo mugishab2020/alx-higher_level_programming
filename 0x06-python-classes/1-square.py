64dd2110c24042665049bdfe602f1de9dcf4aa65
mugishab2020 <mugishab2020@gmail.com>
2023-06-27 05:02:53 -0700

    Task for defining the class is done

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""we are defining the class we provided in task 0"""
+
+
+class Square:
+    def __init__(self, size):
+        """This is the __init__ function that is for self initiation of vars"""
+        self.__size = size
