240b0ee80bf1227fbfc4011166cb6e8b218702ed
mugishab2020 <mugishab2020@gmail.com>
2023-07-05 11:35:09 -0700

    Now we are getting

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" function that prints a text with new lines """
+
+
+def text_indentation(text):
+    last = " "
+    string = ""
+    if text is "":
+        print(string, end='')
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    for i in text:
+        if i is last and i is ' ':
+            last = i
+            continue
+        if (last is '.' or last is '?' or last is ':') and i is ' ':
+            last = i
+            continue
+        if i is '.' or i is '?' or i is ':':
+            string += i + "\n" + "\n"
+            last = i
+        else:
+            string += i
+            last = i
+    print(string.rstrip(' '), end="")
