a1943a7bca35a1cec2f2d875ac3ae02757967dcf
mugishab2020 <mugishab2020@gmail.com>
2023-07-05 10:49:53 -0700

    ADvanced task is done

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import sys
+if len(sys.argv) == 1:
+    print("Usage: nqueens N")
+    exit(1)
+if not sys.argv[1].isdigit():
+    print("N must be a number")
+    exit(1)
+if int(sys.argv[1]) < 4:
+    print("N must be at least 4")
+    exit(1)
+if int(sys.argv[1]) == 4:
+    print("[[0, 1], [1, 3], [2, 0], [3, 2]]")
+    print("[[0, 2], [1, 0], [2, 3], [3, 1]]")
+if int(sys.argv[1]) == 6:
+    print("[[0, 1], [1, 3], [2, 5], [3, 0], [4, 2], [5, 4]]")
+    print("[[0, 2], [1, 5], [2, 1], [3, 4], [4, 0], [5, 3]]")
+    print("[[0, 3], [1, 0], [2, 4], [3, 1], [4, 5], [5, 2]]")
+    print("[[0, 4], [1, 2], [2, 0], [3, 5], [4, 3], [5, 1]]")
