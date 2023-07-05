bd09936fea8a920edce0611bde52de7af680cbaf
mugishab2020 <mugishab2020@gmail.com>
2023-07-05 11:02:55 -0700

    Now we are getting confused

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
-"""Now this is the rectangle defined """
+
+"""Create a class Rectangle that defines a rectangle"""
 
 
 class Rectangle:
-    """
-    Private instance attribute:
-    Private instance attribute: height
-    Instantiation width and height: def __init__(self, width=0, height=0)
-    """
-
-    def __init__(self, width, height):
-        self.width = width
+    def __init__(self, width=0, height=0):
         self.height = height
+        self.width = width
 
     @property
     def width(self):
@@ -19,10 +14,11 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if type(value) is not int: 
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
+
         self.__width = value
 
     @property
@@ -35,4 +31,5 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
+
         self.__height = value
