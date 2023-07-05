068ab4f619abc966b1a44c5fe07887f5557ff12f
mugishab2020 <mugishab2020@gmail.com>
2023-07-05 00:24:29 -0700

    Rectangle with nothing

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""THis is the trianglw call we are asked to do """
+
+
+class Rectangle:
+    """
+    we are aked to do nothing in here
+    """
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,2 @@
+Here we are going to make the class based project . simply we can call it Object oriented project
+
