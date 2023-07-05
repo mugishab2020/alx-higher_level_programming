7c28897326951236c99a26715dec11e17106b43f
mugishab2020 <mugishab2020@gmail.com>
2023-07-05 01:45:37 -0700

    More akasamutwe

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+""" more function in rectanngle class """
+
+
+class Rectangle:
+    """ initialize with the width and height with value checks"""
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """ rea claculator """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """ perimeter calculator """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__width) * 2 + (self.__height * 2)
+
+    def __str__(self):
+        string = ""
+        if self.__width == 0 or self.__height == 0:
+            return string
+        for i in range(self.__height):
+            for j in range(self.__width):
+                string += "#"
+            if i is not self.__height - 1:
+                string += "\n"
+        return string
+
+    def __repr__(self):
+        return ("Rectangle({}, {})".format(self.__width, self.__height))
