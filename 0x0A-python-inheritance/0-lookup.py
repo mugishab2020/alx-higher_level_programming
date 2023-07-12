dbbb6312d6a91e1e0e82c063823a830074307d24
mugishab2020 <mugishab2020@gmail.com>
2023-07-12 04:46:12 -0700

    Now we have started the inheritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+this is the function or method to return the attributes
+"""
+
+
+def lookup(obj):
+    """ this is the inner of the method"""
+    return dir(obj)
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,2 @@
+ This is the main readme file for the inheritance in python
+we are goonna deak with it now
