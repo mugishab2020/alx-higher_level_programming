fa3b51b4fdd722775dad03ddcd90e28c360cb8ca
mugishab2020 <mugishab2020@gmail.com>
2023-07-12 08:38:59 -0700

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This is the updated Geometry clas """
+
+
+class BaseGeometry:
+    """ body of the class is here"""
+    def area(self):
+        """The required function which is public """
+        raise Exception("area() is not implemented")
