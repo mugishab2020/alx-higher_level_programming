51cb6fc02ec580181aa532b4daf5756182dc1c02
mugishab2020 <mugishab2020@gmail.com>
2023-07-12 08:17:09 -0700

    Same class or inherit from

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-""" Thiis function check if the object is for the class 
+""" Thiis function check if the object is for thie class
 or for the super classs/ parent class we inherited from
 """
+
+
 def is_kind_of_class(obj, a_class):
     """ this is the inner part of the function"""
     if isinstance(obj, a_class):
