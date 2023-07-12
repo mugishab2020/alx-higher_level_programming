e0198e6122f2f78533b6124ecc6b0da1d7e3d433
mugishab2020 <mugishab2020@gmail.com>
2023-07-12 08:09:36 -0700

    tyring to be solved

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+""" thiss is the function t check if  obj is for the class"""
+
+
 def is_same_class(obj, a_class):
+    """ the inside of the function """
     if type(obj) == a_class:
         return True
     return False
