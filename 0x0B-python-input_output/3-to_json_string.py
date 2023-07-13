2540bb3fe1469d1327ec8637c0a5cb198f3eb5cd
mugishab2020 <mugishab2020@gmail.com>
2023-07-13 11:22:13 -0700

    To JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" JSON deals are on"""
+import json
+"""Now we are on with json in pyhton"""
+
+
+def to_json_string(my_obj):
+    """ here is the function for JSON representation for
+    an object as arrgument"""
+    return json.dumps(my_obj)
