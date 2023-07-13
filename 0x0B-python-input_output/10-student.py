985b054b54ddad90ad493b714af3b41b6772aae5
mugishab2020 <mugishab2020@gmail.com>
2023-07-13 12:14:58 -0700

    stude class to JSONwith filters

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""updated student"""
+
+
+class Student:
+    """ initiatiion of student class"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieving a dictionary
+        representation of a Student instance"""
+        if attrs is None:
+            return self.__dict__
+
+        else:
+            a = {}
+            for attr in attrs:
+                if hasattr(self, attr):
+                    a[attr] = getattr(self, attr)
+            return a
