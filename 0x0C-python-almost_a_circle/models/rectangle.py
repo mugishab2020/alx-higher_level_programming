69c69275c449f6e1683e0215a5f90449760ffdbf
mugishab2020 <mugishab2020@gmail.com>
2023-07-23 13:13:46 -0700

    documantation

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,6 +7,17 @@ __width -> width
 __height -> height
 __x -> x
 __y -> y
+Args:
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
+            x (int): The x coordinate of the new Rectangle.
+            y (int): The y coordinate of the new Rectangle.
+            id (int): The identity of the new Rectangle.
+        Raises:
+            TypeError: If either of width or height is not an int.
+            ValueError: If either of width or height <= 0.
+            TypeError: If either of x or y is not an int.
+            ValueError: If either of x or y < 0.
 """
 
 
