17d3f46bb8d4c4c53bdf5217a0affa1cc29bbd19
mugishab2020 <mugishab2020@gmail.com>
2023-08-17 13:49:53 -0700

    Listing the previlages

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
--- listing the previlages the user have
-SHOW GRANTS FOR  'user_0d_1'@'localhost';
-SHOW GRANTS FOR  'user_0d_2'@'localhost';
+-- show privilege from user 1 and user 2
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
