d912a3aed05679378ccfb1a36e91bd8f346260c1
mugishab2020 <mugishab2020@gmail.com>
2023-08-18 04:58:49 -0700

    We have citites

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,8 +1,10 @@
 -- creating Db if not exists
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
--- using the hbtn_0d_usa database
+-- use a database
 USE hbtn_0d_usa;
--- creating the table in it
-CREATEA TABLE IF NOT EXISTS cities(id INT UNIQUE AUTO_INCREMENT NOT NULL, state_id INT NOT NULL, name VARCHAR(256) NOT NULL,
+-- creates a table
+CREATE TABLE IF NOT EXISTS cities (id INT UNIQUE AUTO_INCREMENT NOT NULL,
+state_id INT NOT NULL,
+name VARCHAR(256) NOT NULL,
 PRIMARY KEY(id),
 FOREIGN KEY(state_id) REFERENCES states(id));
