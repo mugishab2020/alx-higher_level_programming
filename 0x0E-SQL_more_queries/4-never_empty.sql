4ffb22b6935cda39dc4fd3e0a1901f997f796a4a
mugishab2020 <mugishab2020@gmail.com>
2023-08-18 04:36:20 -0700

    Try to solve them

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,2 +1,2 @@
--- creates a table in a database with ntot nul constraint
-CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
+-- creates a table in a database
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256))
