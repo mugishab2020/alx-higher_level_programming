29e74ae5988201ee4600ebf3ba2920458eee1397
mugishab2020 <mugishab2020@gmail.com>
2023-09-12 20:17:18 +0200

    Still trying

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,10 @@
 #!/usr/bin/node
+let newlist = '';
 exports.esrever = function (list) {
   for (let a = 0; a < list.length; a++) {
+    newlist = list[a];
     list[a] = list[list.length - a - 1];
+    list[list.length - a - 1] = newlist;
   }
   return list;
 };
