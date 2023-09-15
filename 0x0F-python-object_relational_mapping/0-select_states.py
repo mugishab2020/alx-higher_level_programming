77cf3d0a0c5def2c1034d81f25ca90b8890467df
mugishab2020 <mugishab2020@gmail.com>
2023-09-14 23:04:45 -0700

    Hard thing

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
-#importing the python version 3
+# importing the python version 3
 
 if __name__ == "__main__":
-    
     import MySQLdb
     from sys import argv
-    #opening of database
-    database = MySQLdb.connect(host='localhost', user=argv[1], password=argv[2], db=argv[3], port=3306)
-    #declaring the cursor
+    # opening of database
+    database = MySQLdb.connect(
+            host='localhost',
+            user=argv[1],
+            password=argv[2],
+            db=argv[3],
+            port=3306
+            )
+    # declaring the cursor
     curse = database.cursor()
-    #executing the Querry
+    # executing the Querry
     curse.execute("SELECT * FROM states ORDER BY states.id ASC;")
-    #fetching data
+    # fetching data
     data = curse.fetchall()
-    #printing the elements
+    # printing the elements
     for x in data:
-        print (x)
-    #closing cursor
+        print(x)
+    # closing cursor
     curse.close()
-    #clossing database
+    # clossing database
     database.close()
-
-
