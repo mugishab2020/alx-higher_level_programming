90b9d802480b089277ee512c928991ab7f16c8da
mugishab2020 <mugishab2020@gmail.com>
2023-09-16 23:11:57 -0700

    Still hard

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python3
-
+"""
+iporting python3
+"""
 if __name__ == "__main__":
     import MySQLdb
     import sys
-    # declaring
+
+    # declaring varables
     my_host = 'localhost'
     users = sys.argv[1]
     my_password = sys.argv[2]
     my_db = sys.argv[3]
     port = 3306
+
     # connect the db
     database = MySQLdb.connect(
             host=my_host, user=users, passwd=my_password, db=my_db, port=port
             )
     # create the cusror && execute the query
     corse = database.cursor()
+    # executing the query
     corse.execute("SELECT * FROM states WHERE name \
 LIKE BINARY 'N%' ORDER BY id ASC")
 
@@ -24,5 +29,7 @@ LIKE BINARY 'N%' ORDER BY id ASC")
     # print a tuple
     for data in my_data:
         print(data)
+    # closing the cursor
     corse.close()
+    # closing the db
     database.close()
