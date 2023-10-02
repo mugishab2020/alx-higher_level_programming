f8b90fa6d8b266a55c416de2559d580f2b56d577
mugishab2020 <mugishab2020@gmail.com>
2023-10-02 14:01:44 -0700

    Pshing last

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,22 +1,17 @@
 #!/usr/bin/python3
-""" Holberton School staff """
-import sys
+"""using the GitHub API to list commits"""
+
+from sys import argv
 import requests
-
-
-def main():
-    """ def com """
-    owner = sys.argv[1]
-    repo = sys.argv[2]
-    limit = 10
-    url = f'https://api.github.com/repos\
-/{repo}/{owner}/commits?per_page={limit}'
-
-    response = requests.get(url).json()
-    for commit in response:
-        name = commit.get("commit").get("author").get("name")
-        print(f'{commit.get("sha")}: {name}')
-
-
 if __name__ == "__main__":
-    main()
+    repo_name = argv[1]
+    owner = argv[2]
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(
+        owner, repo_name)
+    params = {'per_page': 10}
+    response = requests.get(url, params=params)
+    commits = response.json()
+    for commit in commits:
+        sha = commit['sha']
+        author_name = commit['commit']['author']['name']
+        print(f"{sha}: {author_name}")
