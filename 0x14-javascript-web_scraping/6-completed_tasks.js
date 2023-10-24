f2afcc58123f8648f84127d11664a1fa3a1ca142
mugishab2020 <mugishab2020@gmail.com>
2023-10-24 15:04:04 -0700

    Now wehave the number of completed tasks

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+// importing module
+const request = require('request');
+
+// The API URL
+const apiUrl = process.argv[2];
+const dictionary = {};
+
+// Making HTTP GET request to the API URL
+request(apiUrl, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    const data = JSON.parse(body);
+    data.forEach(function (result) {
+      if (result.completed === true) {
+        const userid = result.userId;
+        if (!(userid in dictionary)) {
+          dictionary[userid] = 0;
+        }
+        dictionary[userid] += 1;
+      }
+    });
+    console.log(dictionary);
+  }
+});
