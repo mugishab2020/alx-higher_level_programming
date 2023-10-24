2ce9ad2318697ae1bc21622475520c1f35e5021c
mugishab2020 <mugishab2020@gmail.com>
2023-10-24 15:07:11 -0700

    styling number 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,9 +3,10 @@
 //modules requesting
 const fs = require('fs');
 
+// file path
 const file = process.argv[2];
 
-
+// file reading
 fs.readFile(file, 'utf-8', (err, data) => {
 	if (err) {
 		console.error('Error reading file:', err);
